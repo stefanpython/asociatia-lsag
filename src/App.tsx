4d871b9d1097f4cee72b0d0659ebb39cdbd888d8
stefanpython <stefanhtml@yahoo.com>
2025-02-03 12:33:57 +0200

    Initiate about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Carousel />
+      <About />
     </>
   );
 }

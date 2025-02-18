eedb0c5b70c6daea2271de7774817f47da311991
stefanpython <stefanhtml@yahoo.com>
2025-02-18 13:24:37 +0200

    Fix ts error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate, useLocation } from "react-router-dom";
 
